5d8b24c95787e83d15416fef01d92f1351a8a3cf
Rohan2427 <rohansonwane8@gmail.com>
2024-10-31 05:35:21 -0700

    space complexity

---
+++ summemory.py
@@ -0,0 +1,28 @@
+# sum of first n numbers
+import tracemalloc as tm
+
+# functions
+  # solution1
+
+def sumofN(n):
+    sum = 0
+    for p in range(1,n+1):
+        sum=sum+p
+    return sum
+
+'''
+  #solution2
+def sumofN(n):
+    return int(n*(n+1))/2
+'''
+
+# programs
+tm.start()
+
+t = sumofN(888)
+
+
+print(t)
+print(tm.get_traced_memory())
+tm.stop()
+
