9ee71821586ac3b5d9ccce4f3966bf4523d2da51
Rohan2427 <rohansonwane8@gmail.com>
2024-10-31 06:29:55 -0700

    space complexity comparision

--- summemory.py
+++ summemory.py
@@ -3,23 +3,23 @@ import tracemalloc as tm
 
 # functions
   # solution1
-
+'''
 def sumofN(n):
     sum = 0
     for p in range(1,n+1):
         sum=sum+p
     return sum
-
 '''
+
   #solution2
 def sumofN(n):
     return int(n*(n+1))/2
-'''
+
 
 # programs
 tm.start()
 
-t = sumofN(888)
+t = sumofN(80000)
 
 
 print(t)
