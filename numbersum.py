7fad34adef7165428e722d47ca28ddc3d4b314bf
Rohan2427 <rohansonwane8@gmail.com>
2024-10-31 03:12:57 -0700

    Edited algo

--- numbersum.py
+++ numbersum.py
@@ -2,15 +2,20 @@
 
 # functions
   # solution1
-
+'''
 def sumofN(n):
     sum = 0
     for p in range(1,n+1):
         sum=sum+p
     return sum
+'''
 
-
+  #solution2
+def sumofN(n):
+    return int(n*(n+1))/2
 # programs
 
 t = sumofN(8)
 print(t)
+
+
