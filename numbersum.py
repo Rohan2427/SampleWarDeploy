33f96a758db9064aa13be72ac0964740872fb8d0
Rohan2427 <rohansonwane8@gmail.com>
2024-10-31 04:56:44 -0700

    calculated time complexity

--- numbersum.py
+++ numbersum.py
@@ -3,23 +3,23 @@ from time import time
 
 # functions
   # solution1
-
+'''
 def sumofN(n):
     sum = 0
     for p in range(1,n+1):
         sum=sum+p
     return sum
-
 '''
+
   #solution2
 def sumofN(n):
     return int(n*(n+1))/2
-'''
+
 
 # programs
 
 s_time=time()
-t = sumofN(8888888)
+t = sumofN(888888888)
 e_time=time()
 d_time=e_time-s_time
 print(t)
