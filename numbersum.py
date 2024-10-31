10690ce12f99a3268c5d127140f90b8a16b27ad2
Rohan2427 <rohansonwane8@gmail.com>
2024-10-31 03:30:02 -0700

    time complexity

--- numbersum.py
+++ numbersum.py
@@ -1,21 +1,28 @@
 # sum of first n numbers
+from time import time
 
 # functions
   # solution1
-'''
+
 def sumofN(n):
     sum = 0
     for p in range(1,n+1):
         sum=sum+p
     return sum
-'''
 
+'''
   #solution2
 def sumofN(n):
     return int(n*(n+1))/2
+'''
+
 # programs
 
-t = sumofN(8)
+s_time=time()
+t = sumofN(8888888)
+e_time=time()
+d_time=e_time-s_time
 print(t)
+print(d_time)
 
 
